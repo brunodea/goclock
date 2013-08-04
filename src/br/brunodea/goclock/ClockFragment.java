a7cd6b7b1c42b36ef928ee2a67e07ef60355136e
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-08-03 23:55:07 -0300

    adicionei um countdowntimer ao ClockFragment

--- src/br/brunodea/goclock/ClockFragment.java
+++ src/br/brunodea/goclock/ClockFragment.java
@@ -1,15 +1,42 @@
 package br.brunodea.goclock;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ClockFragment extends Fragment {
+	
+	private TextView mTextViewCurrentTime;
+	private CountDownTimer mCountDownTimer;
+	
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceBundle) {
-		return inflater.inflate(R.layout.clock, container, false);
+		View v = inflater.inflate(R.layout.clock, container, false);
+		mTextViewCurrentTime = (TextView) v.findViewById(R.id.textview_current_time);
+		
+		final SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+		mCountDownTimer = new CountDownTimer(TimeSettings.MAIN_TIME_SEC, 1000) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+				Date d = new Date(millisUntilFinished);
+				mTextViewCurrentTime.setText(format.format(d));
+			}
+			
+			@Override
+			public void onFinish() {
+			}
+		};
+		
+		
+		return v;
 	}
 }
