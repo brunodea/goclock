5f3c3663c13a9f42839089f0d98b0530c2fbc274
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-08-04 16:18:40 -0300

    adicionei o CanadianTimeRule.

---
+++ src/br/brunodea/goclock/timerule/CanadianTimeRule.java
@@ -0,0 +1,42 @@
+package br.brunodea.goclock.timerule;
+
+import br.brunodea.goclock.App;
+import br.brunodea.goclock.R;
+
+public class CanadianTimeRule extends TimeRule {
+
+	private int mStonesPerByoYomi;
+	private int mCurrStones;
+	
+	public CanadianTimeRule(long milliSMainTime, 
+			long milliSByoYomiTimeMilliS, int stones) {
+		super(milliSMainTime, milliSByoYomiTimeMilliS);
+		mStonesPerByoYomi = stones;
+		mCurrStones = mStonesPerByoYomi;
+	}
+
+	@Override
+	public void onByoYomiTimeOver() {
+		if(mCurrStones > 0) {
+			mTimeOver = true;
+		}
+	}
+
+	@Override
+	public long onPause(long millisUntilFinished) {
+		if(isMainTimeOver()) {
+			mCurrStones -= 1;
+			if(mCurrStones == 0) {
+				mCurrStones = mStonesPerByoYomi;
+				return getByoYomiTime();
+			}
+		}
+		return millisUntilFinished;
+	}
+
+	@Override
+	public String byoYomiInfo() {
+		return App.instance().getString(R.string.stones_left)+": "+mCurrStones;
+	}
+
+}
