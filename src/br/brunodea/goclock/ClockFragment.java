15462b4074c1c7eb944cf95d3605bd8e599766f0
fazevedodev <fazevedo.dev@gmail.com>
2013-08-10 14:27:11 -0300

    Resolvido problema de crash quando usuario aperta back enquanto o relogio esta em execucao

--- src/br/brunodea/goclock/ClockFragment.java
+++ src/br/brunodea/goclock/ClockFragment.java
@@ -132,8 +132,8 @@ public class ClockFragment extends Fragment {
 	
 	@Override
 	public void onDestroy() {
-		super.onDestroy();
 		pauseTimer();
+		super.onDestroy();
 	}
 	
 	private void initialTextValues() {
